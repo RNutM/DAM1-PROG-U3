749c2765989538b347d259702218fb448dfb64d1
Robert Gutiérrez <jrgutinez@gmail.com>
2021-10-11 21:57:05 +0200

    var updates

---
+++ Unidad3/src/Ejercicios/Ejercicio02.java
@@ -0,0 +1,28 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio02 {
+	/**
+	 * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while.
+	 * 
+	 * @author Robert G
+	 */
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int numero = 5;
+		int i = 1;
+
+		System.out.println("Son multiplos de 5 los siguientes números: ");
+		System.out.println("");
+
+		while (i < 21) {
+			System.out.print(+(numero * i) + " ");
+			System.out.print("");
+			i++;
+		}
+		sc.close();
+	}
+}
